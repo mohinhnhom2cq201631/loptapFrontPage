f956e7fde365a6b0e16e34e7a92c7061cfa76ad5
phuongnghi1704 <nghinguyen.170498@gmail.com>
2019-12-26 17:12:35 +0700

    commit cart

--- routes/users.js
+++ routes/users.js
@@ -49,36 +49,34 @@ router.post('/sign-up', passport.authenticate('local.signup', {
     failureFlash: true
 }));
 
-// router.get('/check-out',isLoggedIn,function(req,res,next) {
-//     const brandList = brandDAO.get_Brand_List()
-//     res.render('user/checkout',{
-//         pageTitle: 'Thanh toán COD',
-//         brandList: await brandList,
-//         curCustomer: req.user
-//     })
-// })
+router.get('/check-out',isLoggedIn,function(req,res,next) {
+    res.render('user/checkout',{
+        pageTitle: 'Thanh toán COD',
+        curCustomer: req.user
+    })
+})
 
-// router.post('/checkout',isLoggedIn,function(req,res,next) {
-//     if(!req.session.cart){
-//         res.redirect('/cart');
-//     }
-//     const cart = new Cart(req.session.cart);
-//     var order = new Order({
-//         _id: new mongoose.Types.ObjectId(),
-//         user: req.user._id,
-//         cart: cart,
-//         payment:'Ship COD',
-//         created: new Date().toLocaleDateString(),
-//         status: 'Chưa giao'
-//     });
+router.post('/checkoutCOD',isLoggedIn,function(req,res,next) {
+    if(!req.session.cart){
+        res.redirect('/cart');
+    }
+    const cart = new Cart(req.session.cart);
+    var order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        user: req.user._id,
+        cart: cart,
+        payment:'Ship COD',
+        created: new Date().toLocaleDateString(),
+        status: 'Chưa giao'
+    });
 
-//     order.save(function(error){
-//         if(error) throw error;
-//         console.log("done")
-//         req.session.cart = null;
-//         req.redirect('/')
-//     });   
-// })
+    order.save(function(error){
+        if(error) throw error;
+        console.log("done")
+        req.session.cart = null;
+        req.redirect('/')
+    });   
+})
 module.exports = router;
 
 function isLoggedIn(req, res, next) {
